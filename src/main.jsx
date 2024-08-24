d11d742fea89649fd0416024dc5a278281e9d87c
Priya Singh <72970648+singhpriya147@users.noreply.github.com>
2024-08-24 22:27:37 +0530

    changes in file name

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import Context from './context.jsx'
+import Context from './Context.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
